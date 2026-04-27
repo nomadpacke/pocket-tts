857c1ed67997c31f0fcb0befaf4a54f37be0735e
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2026-04-27 17:28:34 +0200

    saved

--- pocket_tts/modules/layer_scale.py
+++ pocket_tts/modules/layer_scale.py
@@ -1,11 +1,40 @@
 import torch
 import torch.nn as nn
+from max import nn as max_nn
+from max.dtype import DType
+from max.graph import DeviceRef, TensorType, Weight
 
 
-class LayerScale(nn.Module):
+class OldLayerScale(nn.Module):
     def __init__(self, channels: int, init: float):
         super().__init__()
         self.scale = nn.Parameter(torch.full((channels,), init))
 
     def forward(self, x: torch.Tensor):
         return self.scale * x
+
+
+class LayerScale(max_nn.Module):
+    def __init__(self, channels: int, init: float):
+        super().__init__()
+        self.channels = channels
+        self.scale = Weight("scale", DType.float32, (channels,), DeviceRef.CPU())
+        self.compiled_forward = None
+
+    def forward_max(self, x: torch.Tensor):
+        current = self.scale * x
+        return current
+
+    def __call__(self, x: torch.Tensor):
+        if self.compiled_forward is None and isinstance(x, torch.Tensor):
+            self.compiled_forward = self.compile(
+                TensorType(
+                    DType.float32, ("batch", self.channels, "other_dim"), device=DeviceRef.CPU()
+                )
+            )
+
+        if isinstance(x, torch.Tensor):
+            return self.compiled_forward(x)
+
+        current = self.forward_max(x)
+        return current
